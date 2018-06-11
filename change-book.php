29c81fffb193bf8cc82d175253d839464dc8ff64
Chen Yi <34450277+realchenyi@users.noreply.github.com>
2018-06-11 11:05:48 +0800

    增添了管理员搜索功能

--- change-book.php
+++ change-book.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>图书管理系统-书库管理</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css"  href="css/change.css"/>
+    <link rel="stylesheet" type="text/css"  href="css/refer.css"/>
     <link rel="stylesheet" type="text/css"  href="css/bootstrap.css" />
     <script src="jquery/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -23,6 +23,7 @@
 <!-- 引入中文语言包 -->
 <script src="https://cdn.bootcss.com/bootstrap-table/1.11.1/locale/bootstrap-table-zh-CN.min.js"></script>
 </head>
+</head>
 <body>
     <div class="navbar navbar-duomi navbar-static-top" role="navigation">
                 <div class="container-fluid">
@@ -33,7 +34,7 @@
                 </div>
     </div>
     
-    <div class="container-fluid">
+    <div class="container-fluid" >
                 <div class="row">
                     <div id="nav-up" class="col-md-2">
                         <ul id="main-nav" class="nav nav-tabs nav-stacked" >
@@ -73,33 +74,165 @@
                                     <div class="panel panel-default">  
                                         <div class="panel-heading">查询条件</div>  
                                         <div class="panel-body">  
-                                            <form class="form-inline">  
+                                            <form class="form-inline" method="post">  
                                                 <div class="row">  
                                                     <div class="col-sm-4">  
                                                         <label class="control-label">图书名称：</label>  
-                                                        <input id="txtTitle" type="text" class="form-control">  
+                                                        <input id="txtTitle" type="text" class="form-control" name="name">  
                                                     </div>  
                                                     <div class="col-sm-4">  
                                                         <label class="control-label">图书作者：</label>  
-                                                        <input id="txtAuthor" type="text" class="form-control">  
+                                                        <input id="txtAuthor" type="text" class="form-control" name="author">  
                                                     </div>  
                                                     <div class="col-sm-4">  
                                                         <label class="control-label">ISBN：</label>  
-                                                        <input id="txtPublish" type="text" class="form-control">  
+                                                        <input id="txtPublish" type="text" class="form-control" name="ISBN">  
                                                     </div>  
                                                 </div>  
                                   
                                                 <div class="row text-right" style="margin-top:20px;">  
                                                     <div class="col-sm-12">  
-                                                        <input class="btn btn-primary" type="button" value="查询" onclick="SearchData()">  
+                                                        <input class="btn btn-primary" type="submit" value="查询" onclick="SearchData()">  
                                                         <button class="btn btn-success" type="button"><a style='color:rgb(255, 255, 255)' href="./create-book.php">创建新书</a></button>  
                                                     </div>  
                                                 </div>  
                                             </form>  
                                         </div>  
-                                    </div>  
-                                  
-                                <div class="book">
+                                    </div>
+
+
+
+<!-- 管理员搜索功能 !-->                                                                           
+<?php
+
+
+error_reporting(0);
+$name = $_POST['name'];
+$author = $_POST['author'];
+$ISBN = $_POST['ISBN'];
+
+$dbc = mysqli_connect('localhost','root','','book_manager');
+$query = "SELECT * FROM book_info WHERE name = '$name' OR author = '$author' OR ISBN = '$ISBN'";
+$result = mysqli_query($dbc,$query) or die("error quering database". mysqli_error($dbc));
+
+if($_POST) 
+{  
+    if (mysqli_num_rows($result) )
+    {     echo '<div class="container body-content">';
+          echo 
+          '<fieldset>
+          <legend>查询结果</legend>
+          </fieldset> '; 
+          echo '<table class="table table-striped table-hover table-responsive">';
+          echo '<thead> 
+          <tr>
+          <th>图书id</th>
+          <th>书名</th>
+          <th>作者</th>
+          <th>ISBN</th>
+          <th>出版社</th>
+          <th>状态</th>
+          </tr>  
+          </thead>
+          </tbody>
+          ';
+          while ($row = mysqli_fetch_array($result)) 
+        {
+        
+          echo '<tr>';
+          echo '<td>' . $row['book_id'] .  '</td>';
+          echo '<td>' . $row['name'] .  '</td>';
+          echo '<td>' . $row['author'] . '</td>';
+          echo '<td>' . $row['ISBN'] . '</td>';
+          echo '<td>' . $row['press'] . '</td>';
+          echo '<td>' . $row['state'] . '</td>';
+          echo '<td><a class="btn btn-primary" href="edit-book.php?book_id='.$row['book_id'].'" >修改内容</a>
+                                                        <a class="btn btn-info" href="state/delete.php?book_id='.$row['book_id'].'" >下架删除</a>
+                                                        </td>';
+
+
+          echo '</tr>';
+          echo "</tbody>";
+         
+
+
+
+
+        }
+        
+
+        echo '</table>';
+        echo '</div>';
+
+    }  
+    
+
+
+
+    else
+    {
+        echo "<script>alert('无所查图书！');history.go(-1);</script>";  
+    }
+}
+
+echo '<div class="theme-popover" style="display: none;">
+             <div class="theme-poptit">
+                  <a href="javascript:;" title="关闭" class="close">×</a>
+                  <h3>图书详情</h3>
+             </div>
+             <div class="theme-popbod dform">
+                   <form class="theme-signin" name="loginform" action="" method="post">
+                        <ul>
+                             <li><strong>书名：' .$row["name"].'</strong></li>
+                             <li><strong>作者：' .$row["author"].'</strong></li>
+                             <li><strong>ISBN：' .$row["ISBN"].'</strong></li>
+                             <li><strong>编号：' .$row["book_id"].'</strong></li>
+                             <li><strong>出版社：'.$row["press"].'</strong></li>
+                             <li><strong>图书简介：'.$row["introduction"].'</strong><small></small></li>
+                         </ul>
+                   </form>
+            </div>
+    </div>
+         
+    <div class="theme-popover-mask" style="display: none;"></div>';
+
+
+?>
+
+
+
+
+
+
+
+
+ <script>
+
+                jQuery(document).ready(function($) {
+                    $('.theme-login').click(function(){
+                        $('.theme-popover-mask').fadeIn(100);
+                        $('.theme-popover').slideDown(200);
+                    })
+                    $('.theme-poptit .close').click(function(){
+                        $('.theme-popover-mask').fadeOut(100);
+                        $('.theme-popover').slideUp(200);
+                    })
+                
+                })
+ </script>
+
+
+ 
+ <script>
+    function apply()
+    {
+        alert('已成功提交申请，等待管理员审核！')
+        window.location.href = 'user-approval.php';
+    }
+</script>
+
+<!--书库管理!-->
+<div class ="container body-content">
                                     <!--<table id="table"></table>--> 
                                     <fieldset>
                                             <legend>书库管理</legend>
@@ -133,7 +266,7 @@
                                                     echo '<td>' . $row['author'] .  '</td>';
                                                     echo '<td>' . $row['ISBN'] .  '</td>';      
                                                     echo '<td>' . $row['press'] .  '</td>'; 
-                                                   echo '<td>' . $row['state'] .  '</td>';  
+                                                    echo '<td>' . $row['state'] .  '</td>';  
                                                     echo '<td><a class="btn btn-primary" href="edit-book.php?book_id='.$row['book_id'].'" >修改内容</a>
                                                         <a class="btn btn-info" href="state/delete.php?book_id='.$row['book_id'].'" >下架删除</a>
                                                         </td>';
@@ -141,13 +274,9 @@
                                                     echo ' </tbody> '; 
                                         }}
                                         ?> 
-                                 
+                                 </thead>
                                     </table>
                                 </div>  
-                            </div>  
-                    </div>
-                </div>
-    </div>
     
 </body>
 </html>
